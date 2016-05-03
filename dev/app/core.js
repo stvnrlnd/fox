40083c2a7a80aa85de6cbbf7bd53b16d786ee2b3
Steven Roland <steven@mojave.io>
2016-05-03 19:19:02 -0400

    It's me!
    - Added some mock user data.
    - Created an angular service to bring in the data.

--- dev/app/core.js
+++ dev/app/core.js
@@ -1,7 +1,16 @@
 (function(){
   'use strict';
   angular.module('fox', [])
-    .controller('generalController', function($scope) {
-      $scope.beautiful = "beautiful";
+    .controller('generalController', function($scope, infoService) {
+      infoService.getInfo(function(response) {
+        $scope.user = response.data;
+        console.log(response.data);
+      });
+    })
+    .service('infoService', function($http){
+      this.getInfo = function(callback) {
+        $http.get('app/mock/user.json')
+        .then(callback);
+      };
     });
 })();
