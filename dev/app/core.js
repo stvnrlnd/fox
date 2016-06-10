c6b26e33798a1a438c2af1287a229033d3357189
Steven Roland <steven@mojave.io>
2016-06-09 21:14:37 -0400

    Clarifying information
    - Made it clearer what sort of information was being retrieved.

--- dev/app/core.js
+++ dev/app/core.js
@@ -2,13 +2,13 @@
   'use strict';
   angular.module('fox', [])
     .controller('generalController', function($scope, infoService) {
-      infoService.getInfo(function(response) {
-        $scope.user = response.data;
+      infoService.getBasicInfo(function(response) {
+        $scope.userBasic = response.data;
         console.log(response.data);
       });
     })
     .service('infoService', function($http){
-      this.getInfo = function(callback) {
+      this.getBasicInfo = function(callback) {
         $http.get('app/mock/user.json')
         .then(callback);
       };
